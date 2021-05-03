7ec957aebda9fad87f8026015c396b618bc5c1b2
ariellongoria <ariello97@hotmail.com>
2021-05-02 20:39:39 -0500

    finished with JS and HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,13 @@ const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-input");
 const currentWeather = document.querySelector("#current-weather");
 const forecastSection = document.querySelector("#forecast-weather");
+const searchHistoryEl = document.querySelector("#search-history");
+
+let searchHistory = [];
 
 let getWeatherData = function (city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ed354f779244a6744d2f5aac5c1c88cd")
+    let cityName = city[0].toUpperCase() + city.substring(1);
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=ed354f779244a6744d2f5aac5c1c88cd")
         .then(function (response) {
             return response.json();
         })
@@ -22,9 +26,38 @@ let getWeatherData = function (city) {
                     return response.json();
                 })
                 .then(function (data) {
+                    createCurrentWeather(data, cityName);
                     createForecastCards(data);
                 });
         });
+};
+
+let createCurrentWeather = function (weatherData, city) {
+    currentWeather.textContent = "";
+    let currentDate = document.createElement("h2");
+    let currentTemp = document.createElement("p");
+    let currentWind = document.createElement("p");
+    let currentHumidity = document.createElement("p");
+    let currentUvi = document.createElement("p");
+
+    currentDate.innerHTML =
+        city +
+        ", " +
+        dateConvert(weatherData.current.dt) +
+        " " +
+        "<img src='http://openweathermap.org/img/wn/" +
+        weatherData.current.weather[0].icon +
+        "@2x.png'/>";
+    currentTemp.innerHTML = "Temperature: " + weatherData.current.temp + " &deg;F";
+    currentWind.textContent = "Wind Speed: " + weatherData.current.wind_speed + "mph";
+    currentHumidity.textContent = "Humidity: " + weatherData.current.humidity + "%";
+    currentUvi.textContent = "UV Index: " + weatherData.current.uvi;
+
+    currentWeather.appendChild(currentDate);
+    currentWeather.appendChild(currentTemp);
+    currentWeather.appendChild(currentWind);
+    currentWeather.appendChild(currentHumidity);
+    currentWeather.appendChild(currentUvi);
 };
 
 let createForecastCards = function (weatherData) {
@@ -32,17 +65,23 @@ let createForecastCards = function (weatherData) {
     for (let i = 1; i < 6; i++) {
         let forecastDayCard = document.createElement("div");
         let forecastDate = document.createElement("h3");
-        // let forecastIcon
+        let forecastIcon = document.createElement("img");
         let forecastTemp = document.createElement("p");
         let forecastWind = document.createElement("p");
         let forecastHumidity = document.createElement("p");
 
-        forecastDate.textContent = dateConvert(weatherData.daily[i].dt);
-        forecastTemp.textContent = weatherData.daily[i].temp.day;
-        forecastWind.textContent = weatherData.daily[i].wind_speed + " mph";
-        forecastHumidity.textContent = weatherData.daily[i].humidity + "%";
+        forecastDayCard.className = "weather-card";
+        forecastDate.innerHTML = dateConvert(weatherData.daily[i].dt);
+        forecastIcon.setAttribute(
+            "src",
+            "http://openweathermap.org/img/wn/" + weatherData.daily[i].weather[0].icon + "@2x.png"
+        );
+        forecastTemp.innerHTML = "Temperature: " + weatherData.daily[i].temp.day + " &deg;F";
+        forecastWind.textContent = "Wind Speed: " + weatherData.daily[i].wind_speed + " mph";
+        forecastHumidity.textContent = "Humidity: " + weatherData.daily[i].humidity + "%";
 
         forecastDayCard.appendChild(forecastDate);
+        forecastDayCard.appendChild(forecastIcon);
         forecastDayCard.appendChild(forecastTemp);
         forecastDayCard.appendChild(forecastWind);
         forecastDayCard.appendChild(forecastHumidity);
@@ -63,20 +102,42 @@ let dateConvert = function (time) {
     return dateString;
 };
 
+let saveLocations = function () {
+    localStorage.setItem("locations", JSON.stringify(searchHistory));
+};
+
+let loadLocations = function () {
+    searchHistoryEl.textContent = "";
+    searchHistory = JSON.parse(localStorage.getItem("locations")) || [];
+    for (let i = 0; i < searchHistory.length; i++) {
+        let historyEl = document.createElement("button");
+        historyEl.className = "history-btn";
+        historyEl.textContent = searchHistory[i];
+        searchHistoryEl.appendChild(historyEl);
+    }
+};
+
+loadLocations();
+
 searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    getWeatherData(searchInput.value);
+    let cityName = searchInput.value[0].toUpperCase() + searchInput.value.substring(1);
+    if (searchInput.value) {
+        getWeatherData(cityName);
+        if (searchHistory.length < 10) {
+            searchHistory.push(cityName);
+        } else {
+            searchHistory.shift();
+            searchHistory.push(cityName);
+        }
+    }
+    saveLocations();
+    loadLocations();
     searchForm.reset();
 });
 
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+searchHistoryEl.addEventListener("click", function (event) {
+    if (event.target.className === "history-btn") {
+        getWeatherData(event.target.textContent);
+    }
+});
